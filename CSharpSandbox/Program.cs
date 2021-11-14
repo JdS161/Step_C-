8f60baa1bb2f18cf6f574d2e3df478b8dc222994
JdS161 <a.vasiltschenko@gmail.com>
2021-11-14 13:00:58 +0300

    C# lesson2 finished

--- CSharpSandbox/Program.cs
+++ CSharpSandbox/Program.cs
@@ -46,18 +46,188 @@ namespace CSharpSandbox
             //Console.WriteLine(b);
 
 
-            //ax + b = 0
+            ////ax + b = 0
 
-            double a, b;
-            Console.Write("Write down a: ");
-            a = double.Parse(Console.ReadLine());
-            Console.Write("Write down b: ");
-            b = Convert.ToDouble(Console.ReadLine());
-            double x = -b / a;
-            Console.WriteLine(x);  
+            //double a, b;
+            //Console.Write("Write down a: ");
+            //a = double.Parse(Console.ReadLine());
+            //Console.Write("Write down b: ");
+            //b = Convert.ToDouble(Console.ReadLine());
+            //double x = -b / a;
+            //Console.WriteLine(x);  
 
-            Console.WriteLine("x = {0}; a = {1}, b = {2}", x, a, b);
-            Console.WriteLine($"x = {x}; a = {a}, b = {b}");
+            //Console.WriteLine("x = {0}; a = {1}, b = {2}", x, a, b);
+            //Console.WriteLine($"x = {x}; a = {a}, b = {b}");
+
+
+            //int a = 0b1010;
+            //Console.WriteLine(a);
+
+
+
+
+            //double a, b, x;
+            //Console.Write("A = ");
+            //a = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("B = ");
+            //b = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("X = ");
+            //x = Convert.ToDouble(Console.ReadLine());
+
+            //if (a > b)
+            //{
+            //    double temp = a;
+            //    a = b;
+            //    b = temp;
+            //}
+            //if (x>a && x <b)
+            //{
+            //    Console.WriteLine($"Number {x} is in range ({a}, {b})");
+            //}
+            //else if (x ==a || x == b)
+            //{
+            //        Console.WriteLine($"Number {x} is on the border of the range ({a}, {b})");
+            //}
+            //else
+            //{
+            //        Console.WriteLine($"Number {x} is out of the range ({a}, {b})");
+            //}
+
+            //int num;
+            //num = Convert.ToInt32(Console.ReadLine());
+            //bool isSimple = true;
+            //for (int i = 2; i < num && isSimple; i++)
+            //{
+            //    if (num% i == 0)
+            //    {
+            //        isSimple = false;
+            //    }
+            //}
+
+            //if (isSimple)
+            //{
+            //    Console.WriteLine($"Number {num} is simple");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Number {num} is not simple");
+            //}
+            ////isSimple ? Console.WriteLine($"Number {num} is simple") : Console.WriteLine($"Number {num} is not simple"); // TODO: Check this line
+
+            //ARRAYS
+
+            //int[] arr = new int[10];
+            //for (int i=0; i< arr.Length; i++)
+            //{
+            //    Console.Write(arr[i] + " ");
+            //}
+
+
+            //Console.WriteLine();
+
+            //foreach (int elem in arr)
+            //    Console.Write($"{elem} ");
+
+            //Console.WriteLine();
+
+            //int[] marks = new int[8];
+
+            //for (int i = 0; i < marks.Length; i++)
+            //{
+            //    Console.Write("Input a mark");
+            //    marks[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+
+            ////average:
+            //double average = marks.Average();
+            //Console.WriteLine($"Average = {average}");
+
+            ////marks values counter
+            //int[] countMarks = new int[marks.Max() + 1];
+            //for (int i=0; i < countMarks.Length; i++)
+            //{
+            //    countMarks[i] = marks.Count(elem => elem == i);
+            //}
+
+            //for (int  i = 0; i < countMarks.Length; i++)                            //      TODO: UNDERSTAND THIS!!!
+            //{                                                                       //          LINES 145-155
+            //    Console.WriteLine($"Number of marks {i} = {countMarks[i]}");
+            //}
+
+
+
+            //int[] arr_1 = new int[] { 1, 2, 3 };
+            //int[] arr_2 = arr_1;
+            //arr_2[0] = 0;
+            //Console.WriteLine(arr_1[0]);
+
+            ////DEEP COPY
+            //arr_1 = new int[] { -1, -2, -3 };
+            //arr_1.CopyTo(arr_2, 0);
+            //arr_2[0] = 0;
+            //Console.WriteLine(arr_1[0]);
+
+
+            ////rectangular 2-dimensional array
+            //int rows = 3, cols = 5;
+            //double[,] matrix = new double [rows,cols];
+
+            //for (int i = 0; i < rows; i++)
+            //{
+            //    for ( int j=0; j< cols; j++)
+            //    {
+            //        matrix[i, j] = i * j ;
+            //    }
+            //} 
+
+            //for (int i = 0; i < rows; i++)
+            //{
+            //    for ( int j=0; j< cols; j++)
+            //    {
+            //        Console.Write($"{matrix[i, j]} ");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //    Console.WriteLine();
+
+            //foreach(double elem in matrix)
+            //{
+            //        Console.Write($"{elem} ");
+            //}
+            //    Console.WriteLine();
+
+            //triangle array
+            int[][] trArr = new int[3][]; // 3 empty rows created here
+            trArr[0] = new int[1];
+            trArr[1] = new int[2];
+            trArr[2] = new int[3];
+
+
+            for (int i = 0; i < trArr.Length; i++)
+            {
+                for (int j = 0; j < trArr[i].Length; j++)
+                {
+                    trArr[i][j] = i - j - 10;
+                    Console.Write($"{trArr[i][j]} ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            foreach ( int[] str in trArr)
+            {
+                foreach(int elem in str)
+                {
+                    Console.Write($"{elem} ");
+                }
+                Console.WriteLine();
+            }
+
+
+
         }
+
+
+
     }
 }
