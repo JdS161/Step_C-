c985f2fa5428a10271e374b8f0119e5c0255c14a
JdS161 <a.vasiltschenko@gmail.com>
2021-12-06 21:04:53 +0300

    Average and Min functions were added

--- HW6/Program.cs
+++ HW6/Program.cs
@@ -36,46 +36,56 @@ namespace HW6_CSharp
                 marks[i] = new int[] { };
             }
 
-            Console.Write("\nWrite index of the subject, which marks you want to fill in:");
-            int choosenSubj = Convert.ToInt32(Console.ReadLine());
+            bool[] flags = new bool[numSubj];
+            for (int  i = 0;  i <numSubj;  i++)
+            {
+                flags[i] = false;
+            }
+
+            Console.WriteLine("\nPress <Esc> for exit, <Enter> to continue");
+            while(Console.ReadKey().Key != ConsoleKey.Escape)
+            {
+
+
+                Console.Write("\nWrite index of the subject, which marks you want to fill in:");
+                int choosenSubj = Convert.ToInt32(Console.ReadLine());
                         
-            Console.Write("\nHow many marks you want to fill: ");
-            int numMarksAdded = Convert.ToInt32(Console.ReadLine());
-
-            Array.Resize(ref marks[choosenSubj-1], numMarksAdded);
-
-            for (int i=0; i< numMarksAdded; i++)
-            {
-                Console.Write($"{subjects[choosenSubj-1]}'s mark {i + 1}: ");
-                marks[choosenSubj][i] = Convert.ToInt32(Console.ReadLine());
-            }
+                Console.Write("\nHow many marks you want to fill: ");
+                int numMarksAdded = Convert.ToInt32(Console.ReadLine());
 
 
-            System.Console.Clear();
+                FillMarks(ref marks, subjects, ref choosenSubj, ref numMarksAdded, ref flags);
+                PrintTable(ref marks, subjects, numSubj, ref flags);
 
-            char pad = '.';
+                AverageTotal(ref marks, ref flags);
+                Console.WriteLine("\nPress <Esc> for exit, <Enter> to continue");
 
-            for (int i = 0; i < numSubj; i++)
-            {
-                Console.Write($"\t{i + 1} {subjects[i].PadRight(15, pad)} | ");
-                PrintMarks(marks, i);
-                Console.WriteLine();
             }
 
 
 
+            //Console.Write("\nWrite index of the subject, which marks you want to fill in:");
+            //int choosenSubj2 = Convert.ToInt32(Console.ReadLine());
 
-
+            //Console.Write("\nHow many marks you want to fill: ");
+            //int numMarksAdded2 = Convert.ToInt32(Console.ReadLine());
             
 
+            //FillMarks(ref marks, subjects, ref choosenSubj2, ref numMarksAdded2, ref flags);
+
+            //Console.Write("\nWrite index of the subject, which marks you want to fill in:");
+            //int choosenSubj3 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("\nHow many marks you want to fill: ");
+            //int numMarksAdded3 = Convert.ToInt32(Console.ReadLine());
 
 
+            //FillMarks(ref marks, subjects, ref choosenSubj3, ref numMarksAdded3, ref flags);
+
+            //PrintTable(ref marks, subjects, numSubj);
 
 
-
-
-
-
+             
 
 
 
@@ -85,13 +95,116 @@ namespace HW6_CSharp
 
 
         }
-    
-    static void FillMarks()
+        
+        static void AverageDiscipline(ref int[][] _marks, int _choosenSubj, ref bool[] _flags)
+        {
+            if(_flags[_choosenSubj] == false)
+            {
+                Console.Write($" {0:##.0}");
+            }
+            else
+            {
+                Console.Write($"{_marks[_choosenSubj].Average():##.0}");
+
+            }
+
+        }
+        static void AverageTotal(ref int[][] _marks, ref bool[] _flags)
+        {
+            double result = 0.0;
+            for (int i = 0; i <_marks.Length ; i++)
+            {
+                if(_flags[i]==false)
+                {
+                    result += 0.0;
+                }
+                else
+                {
+                    result += _marks[i].Average();
+                }
+            }
+            Console.WriteLine($"Total average: {result / _marks.Length:##.0}");
+        }
+        static void MinMark(ref int[][] _marks, int _choosenSubj, ref bool[] _flags)
+        {
+            int minMark = 5;
+            if(_flags[_choosenSubj] == false)
+            {
+                Console.Write("N/a");
+            }
+            else
+            {
+                for (int i = 0; i < _marks[_choosenSubj].Length; i++)
+                {
+                    if(_marks[_choosenSubj][i] < minMark)
+                    {
+                        minMark = _marks[_choosenSubj][i];
+                    }
+                }
+                Console.Write($" {minMark} ");
+            }
+
+        }
+
+        static void MaxMark()
         {
 
         }
+
+        static void Performance()
+        {
+
+        }
+        static void PrintTable(ref int[][] _marks, string[] _subjects, int _numSubj, ref bool[] _flags)
+        {
+            System.Console.Clear();
+           
+            char pad = '.';
+            Console.WriteLine();
+            for (int i = 0; i < _numSubj; i++)
+            {
+                Console.Write($" {i + 1} {_subjects[i].PadRight(15, pad)} | ");
+                AverageDiscipline(ref _marks, i, ref _flags);
+                Console.Write(" | ");
+                MinMark(ref _marks, i, ref _flags);
+                Console.Write(" | ");
+                PrintMarks(_marks, i);
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        static void FillMarks(ref int[][] _marks,string[] _subjects, ref int _choosenSubj, ref int _numMarksAdded, ref bool[] _flags)
+        {
+            if (_flags[_choosenSubj - 1] == false)
+            {
+                Array.Resize(ref _marks[_choosenSubj - 1], _numMarksAdded);
+
+                for (int i = 0; i < _numMarksAdded; i++)
+                {
+                    Console.Write($"{_subjects[_choosenSubj - 1]}'s mark {i + 1}: ");
+                    _marks[_choosenSubj - 1][i] = Convert.ToInt32(Console.ReadLine());
+
+                }
+                _flags[_choosenSubj - 1] = true;
+            }
+            else
+            {
+                int len = _marks[_choosenSubj - 1].Length;
+                Array.Resize(ref _marks[_choosenSubj - 1], len + _numMarksAdded);
+
+                for (int i = len; i < len+_numMarksAdded; i++)
+                {
+                    Console.Write($"{_subjects[_choosenSubj - 1]}'s mark {i + 1}: ");
+                    _marks[_choosenSubj - 1][i] = Convert.ToInt32(Console.ReadLine());
+
+                }
+                _flags[_choosenSubj - 1] = true;
+            }
+
+        }
       
-    static void PrintMarks(int[][] arr, int _row)
+        static void PrintMarks(int[][] arr, int _row)
         {
             foreach(int item in arr[_row])
             {
