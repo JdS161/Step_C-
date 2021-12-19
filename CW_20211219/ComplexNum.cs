9c46098dd0a2d1068640d5e2194f164855e4f57f
JdS161 <a.vasiltschenko@gmail.com>
2021-12-19 19:18:31 +0300

    Lesson 2021_12_19 DONE

---
+++ CW_20211219/ComplexNum.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW_20211219
+{
+    class ComplexNum
+    {
+        public double Re { get; set; }
+        public double Im { get; set; }
+
+
+        public ComplexNum()
+        {
+            Re = 0;
+            Im = 0;
+        }
+        
+        public ComplexNum(double _re, double _im)
+        {
+            this.Re = _re;
+            this.Im = _im;
+        }
+
+        // OPERATOR OVERLOAD
+        // 1. MULTIPLICATION
+
+        public static ComplexNum operator*(ComplexNum _firstCimplexNum, ComplexNum _secondComplexNum)
+        {
+            double resRe = _firstCimplexNum.Re * _secondComplexNum.Re - _firstCimplexNum.Im * _secondComplexNum.Im; 
+            double resIm = _firstCimplexNum.Re * _secondComplexNum.Im + _firstCimplexNum.Im * _secondComplexNum.Re;
+            return new ComplexNum(resRe, resIm);
+        }
+
+        public static ComplexNum operator*(ComplexNum _complexNum, double _num)
+        {
+            return new ComplexNum(_complexNum.Re * _num, _complexNum.Im * _num);
+        }
+        
+        public static ComplexNum operator*(double _num, ComplexNum _complexNum)
+        {
+            return new ComplexNum(_complexNum.Re * _num, _complexNum.Im * _num);
+        }
+
+        //2. Unary minus overload 
+        public static ComplexNum operator-(ComplexNum _complexNum)
+        {
+            return new ComplexNum(-_complexNum.Re, -_complexNum.Im);
+        }
+        public static ComplexNum operator++(ComplexNum _complexNum)
+        {
+            _complexNum.Re++;
+            _complexNum.Im++;
+            return new ComplexNum(_complexNum.Re, _complexNum.Im);
+        }
+        public static ComplexNum operator--(ComplexNum _complexNum)
+        {
+            _complexNum.Re--;
+            _complexNum.Im--;
+            return new ComplexNum(_complexNum.Re, _complexNum.Im);
+        }
+
+
+        public override string ToString()
+        {
+            return $"{Re} + {Im}i";
+        }
+
+
+
+    }
+}
---
+++ CW_20211219/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CW_20211219
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //-------------------------------------------------------------------------------------------------
+            //--------------------------------------OPERATOR OVERLOADS-----------------------------------------
+            //-------------------------------------------------------------------------------------------------
+
+            ComplexNum firstCoplexNum = new ComplexNum(1, 2);
+            ComplexNum secondCoplexNum = new ComplexNum(3, 4);
+
+            ComplexNum resultComplexNum = firstCoplexNum * secondCoplexNum;
+            double num = 10;
+            ComplexNum resultComplexNum2 = firstCoplexNum * num;
+            double num2 = 20;
+            ComplexNum resultComplexNum3 = num2 * secondCoplexNum;
+
+            Console.WriteLine($"{firstCoplexNum} * {secondCoplexNum} = {resultComplexNum}");
+            Console.WriteLine($"{firstCoplexNum} * {num} = {resultComplexNum2}");
+            Console.WriteLine($"{num2} * {secondCoplexNum} = {resultComplexNum3}");
+            Console.WriteLine($"{firstCoplexNum} = {-firstCoplexNum}");
+
+
+            ComplexNum resultComplexNum4 =firstCoplexNum++;
+            ComplexNum resultComplexNum5 =secondCoplexNum--;
+            Console.WriteLine($"{firstCoplexNum} = incremented {resultComplexNum4}");
+            Console.WriteLine($"{secondCoplexNum} = incremented {resultComplexNum5}");
+
+        }
+    }
+}
