9bbedff1c2de5c5312636151946431cf69af69c3
JdS161 <a.vasiltschenko@gmail.com>
2021-12-26 20:14:05 +0300

    Some additional instances of the class were added for the tests

--- HW8_CSharp/Program.cs
+++ HW8_CSharp/Program.cs
@@ -14,11 +14,11 @@ namespace HW8_CSharp
             // +2.   Реализовать не менее пяти закрытых полей (различных типов), представляющих
             //      основные характеристики рассматриваемого класса.
 
-            // 3.   Создать не менее трех методов управения классом и методы доступа к его закрытым полям.
+            // +3.   Создать не менее трех методов управения классом и методы доступа к его закрытым полям.
 
             // 4.   Создать метод, в который передаются аргументы по ссылке.
 
-            // 5.   Создать не менее двух статических полей (различных типов), представляющих
+            // +5.   Создать не менее двух статических полей (различных типов), представляющих
             //      общие характеристики объектов данного класса.
 
             // 6.   Обязательным требованием является реализация нескольких перегруженных конструкторов,
@@ -29,24 +29,25 @@ namespace HW8_CSharp
 
             // 8.   Создать массив (не менее 5 элементов) объектов созданного класса.
 
-            // 9.   Создать дополнительный метод для данного класса в другом файле, используя ключевое 
+            // +9.   Создать дополнительный метод для данного класса в другом файле, используя ключевое 
             //      слово partial.
 
             CarrierRocket launcher1 = new CarrierRocket();
-            Console.WriteLine(launcher1.ToString());
-            Console.WriteLine();
+           
             CarrierRocket launcher2 = new CarrierRocket("Falcon", 2344,60000,true,true,false,CarrierType.ORBITAL,CountryOfOrigin.USA);
-            Console.WriteLine(launcher2.ToString());
-
-            //Console.WriteLine($"{launcher2.Launch(launcher2)}");
-            //CarrierRocket.Launch(launcher2);
-            //launcher2.Launch(launcher2);
+            CarrierRocket launcher3 = new CarrierRocket("Souz", 2344,34000,false,false,false,CarrierType.SUBORBITAL,CountryOfOrigin.RUSSIA);
+            CarrierRocket launcher4 = new CarrierRocket("Proton", 2344,50000,false,false,false,CarrierType.ORBITAL,CountryOfOrigin.RUSSIA);
+            CarrierRocket launcher5 = new CarrierRocket("StarShip", 2244,6000,true,true,true,CarrierType.INTERSTELLAR,CountryOfOrigin.USA);
+            
 
             launcher2.Launched(launcher2);
 
             CarrierRocketPool group = new CarrierRocketPool();
             group.AddRocket(launcher1);
             group.AddRocket(launcher2);
+            group.AddRocket(launcher3);
+            group.AddRocket(launcher4);
+            group.AddRocket(launcher5);
 
 
             Console.WriteLine(group.ToString());
