60384db59625b92782fe6b688a3cf347573239d0
JdS161 <a.vasiltschenko@gmail.com>
2022-01-09 17:17:03 +0300

    HW12 (operator overloading) - DONE

--- HW12_CSharp/Money.cs
+++ HW12_CSharp/Money.cs
@@ -1,33 +1,146 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using System;
 namespace HW12_CSharp
 {
-    abstract class Money
+    public class Money
     {
-        public string Currency {get; set;}
-        public double Amount{get; set;}
-      
+        public string Currency { get; set; }
+        public double Amount { get; set; }
 
         public Money()
         {
-            Currency  = "NONE";
+            Currency = "NONE";
             Amount = 0;
         }
 
         public Money(string _currency, double _amount)
         {
-            Currency  = _currency;
-            Amount = _amount;            
+            Currency = _currency;
+            Amount = _amount;
+        }
+
+        public static Money operator +(Money _lMoney, Money _rMoney)
+        {
+            Money res = new Money();
+            if(_lMoney.Currency != _rMoney.Currency)
+            {
+                Console.WriteLine("Impossible to sum money with different currencies");
+                return res;
+            }
+            else
+            {
+                res.Amount = _lMoney.Amount + _rMoney.Amount;
+                res.Currency = _lMoney.Currency;
+                return res;
+            }
+        }
+        public static Money operator -(Money _lMoney, Money _rMoney)
+        {
+            Money result = new Money();
+            if (_lMoney.Currency != _rMoney.Currency)
+            {
+                Console.WriteLine("Impossible to subtract money with different currencies");
+                return result;
+            }
+            else
+            {
+                if (_lMoney.Amount > _rMoney.Amount)
+                {
+                    result.Amount = _lMoney.Amount - _rMoney.Amount;
+                }
+                else if (_rMoney.Amount > _lMoney.Amount)
+                {
+                    result.Amount = _rMoney.Amount - _lMoney.Amount;
+                }
+                result.Currency = _lMoney.Currency;
+                return result;
+            }
+        }
+        public static Money operator /(Money _lMoney, int _num)
+        {
+            Money res = new Money();
+            res.Amount = _lMoney.Amount / _num;
+            res.Currency = _lMoney.Currency;
+            return res;
+        }
+        public static Money operator *(Money _lMoney, int _num)
+        {
+            Money res = new Money();
+            res.Amount = _lMoney.Amount * _num;
+            res.Currency = _lMoney.Currency;
+            return res;
+        }
+        public static Money operator ++(Money _lMoney)
+        {
+            Money res = new Money();
+            res.Amount = _lMoney.Amount + 0.01;
+            res.Currency = _lMoney.Currency;
+            return res;
+        }
+        public static Money operator --(Money _lMoney)
+        {
+            Money res = new Money();
+            res.Amount = _lMoney.Amount - 0.01;
+            res.Currency = _lMoney.Currency;
+            return res;
+
+        }
+        public static bool operator >(Money _lMoney, Money _rMoney)
+        {
+            
+            if (_lMoney.Currency != _rMoney.Currency)
+            {
+                Console.WriteLine("Impossible to compare money with different currencies");
+                return false;
+            }
+            else
+            {
+                return _lMoney.Amount > _rMoney.Amount;
+
+            }
+        }
+        public static bool operator <(Money _lMoney, Money _rMoney)
+        {
+
+            if (_lMoney.Currency != _rMoney.Currency)
+            {
+                Console.WriteLine("Impossible to compare money with different currencies");
+                return false;
+            }
+            else
+            {
+                return _lMoney.Amount < _rMoney.Amount;
+
+            }
+        }
+        //public static Money operator <(Money _lMoney, Money _rMoney)
+        //{
+        //    return _lMoney < _rMoney;
+        //}
+
+        public static bool operator ==(Money _lMoney, Money _rMoney)
+        {
+                return _lMoney.Equals(_rMoney);
+        }
+        public static bool operator !=(Money _lMoney, Money _rMoney)
+        {
+            return !_lMoney.Equals(_rMoney);
+        }
+
+
+        public override bool Equals(object obj)
+        {
+            return (obj is Money) && ((Money)obj).Amount == Amount;
+        }
+        public override string ToString()
+        {
+            //double res = Amount;
+            
+            return $"{String.Format("{0:0.00}", Amount)} {Currency}";
+        }
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
         }
         
-        abstract public void Print();
-
-
-
-
     }
 }
--- HW12_CSharp/Program.cs
+++ HW12_CSharp/Program.cs
@@ -10,9 +10,29 @@ namespace HW12_CSharp
             // Для класса реализовать перегрузку операторов +(сложение денежных сумм), -(вычитание сумм), 
             // /(деление суммы на целое число), *(умножение суммы на целое число), ++(сумма увеличивается 
             // на 1 копейку), --(сумма уменьшается на 1 копейку), <, >, ==, !=.
-            Console.WriteLine("That's a modified file! ");
+          
 
-            
+            Money US = new Money("USD", 30);
+            Money US2 = new Money("USD", 40);
+            US++;
+            Money RU = new Money("RUB", 500);
+            Money RU2 = new Money("RUB", 20);
+            RU--;
+            Console.WriteLine(US);
+            Console.WriteLine(RU);
+            Console.WriteLine(RU*3);
+            Console.WriteLine(US/3);
+            Console.WriteLine(US == RU);
+            Console.WriteLine(US != RU);
+            Console.WriteLine(US + US2);
+            Console.WriteLine(RU - RU2);
+            Console.WriteLine(US + RU2);
+            Console.WriteLine(US - RU2);
+            Console.WriteLine(US > RU);
+            Console.WriteLine(US > US2);
+            Console.WriteLine(US < RU);
+            Console.WriteLine(RU2 < RU);
+
         }
     }
 }
