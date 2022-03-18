83b16d456ad88325f6982a546dc3cfba2cd355bc
JdS161 <a.vasiltschenko@gmail.com>
2022-03-18 12:21:41 +0300

    Initial commit of the EXAM project

---
+++ EXAM_Dictionary/Console_UI.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXAM_Dictionary
+{
+    internal class Console_UI
+    {
+    }
+}
---
+++ EXAM_Dictionary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EXAM_Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
